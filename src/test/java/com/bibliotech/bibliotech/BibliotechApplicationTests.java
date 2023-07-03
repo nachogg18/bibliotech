18149e8463f71431a9fc1c68ee9682ae1ce6585e
Ignacio Galvez <iggatsc@gmail.com>
2023-07-03 20:30:33 -0300

    add setup project

---
+++ src/test/java/com/bibliotech/bibliotech/BibliotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotech.bibliotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
