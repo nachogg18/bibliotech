d0df59470d13f7a2f11c64f83bc3b91bab8fccaa
Ignacio Galvez <iggatsc@gmail.com>
2023-07-07 00:20:05 -0300

    reformat code with google-java-format

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bibliotech
+
 bibliotech project
--- src/main/java/com/bibliotech/bibliotech/BibliotechApplication.java
+++ src/main/java/com/bibliotech/bibliotech/BibliotechApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BibliotechApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BibliotechApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BibliotechApplication.class, args);
+  }
 }
