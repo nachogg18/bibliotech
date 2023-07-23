3634998782f9b3c5fb2dc582d774dc7f0da3a36f
Ignacio Valente <ignacio.valente@alumnos.frm.utn.edu.ar>
2023-07-23 20:07:25 -0300

    Add repository annotation to CategoriaRepository

--- src/main/java/com/bibliotech/repository/CategoriaRepository.java
+++ src/main/java/com/bibliotech/repository/CategoriaRepository.java
@@ -2,6 +2,8 @@ package com.bibliotech.repository;
 
 
 import com.bibliotech.entity.Categoria;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoriaRepository extends BaseRepository<Categoria, Long>{
 }
