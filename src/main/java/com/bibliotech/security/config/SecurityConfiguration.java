704631ded9c584ae985e59990894cb2d61ed0967
Ignacio Galvez <iggatsc@gmail.com>
2023-09-12 08:20:20 -0300

    enhancement authentication

--- src/main/java/com/bibliotech/security/config/SecurityConfiguration.java
+++ src/main/java/com/bibliotech/security/config/SecurityConfiguration.java
@@ -27,7 +27,9 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**").permitAll()
+                .authorizeHttpRequests(request -> 
+                        request.requestMatchers("/api/v1/auth/**").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
