a8e57a3e796398a1d101781961328d525355ad04
Sergio Villanueva <65696419+hexdump95@users.noreply.github.com>
2023-09-26 19:55:37 -0300

    Create MultaEstado.java

---
+++ src/main/java/com/bibliotech/entity/MultaEstado.java
@@ -0,0 +1,28 @@
+package com.bibliotech.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+import lombok.*;
+
+@Entity
+@Table(name = "multa_estado")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MultaEstado extends Base {
+    @Column
+    private String nombre;
+    @Column
+    private Instant fechaInicio = Instant.now();
+    @Column
+    private Instant fechaFin;
+    @Column
+    @Enumerated(EnumType.ORDINAL)
+    private EstadoMulta estadoMulta;
+    @ManyToOne
+    private Multa multa;
+}
