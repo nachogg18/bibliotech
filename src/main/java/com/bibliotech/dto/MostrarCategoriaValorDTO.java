33e7fa99276b22724fc5af6bbbd68ffde94a11cb
Sergio Villanueva <sergiovillanueva@protonmail.com>
2023-09-29 15:00:14 -0300

    Fix dto categoria

---
+++ src/main/java/com/bibliotech/dto/MostrarCategoriaValorDTO.java
@@ -0,0 +1,12 @@
+package com.bibliotech.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MostrarCategoriaValorDTO {
+    private String nombre;
+}
--- src/main/java/com/bibliotech/service/CategoriaValorServiceImpl.java
+++ src/main/java/com/bibliotech/service/CategoriaValorServiceImpl.java
@@ -1,24 +1,34 @@
 package com.bibliotech.service;
 
+import com.bibliotech.dto.CrearValorDTO;
+import com.bibliotech.dto.MostrarCategoriaDTO;
+import com.bibliotech.dto.MostrarCategoriaValorDTO;
+import com.bibliotech.entity.Categoria;
 import com.bibliotech.entity.CategoriaValor;
 import com.bibliotech.repository.CategoriaValorRepository;
 
 import java.time.Instant;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
+@Log4j2
 public class CategoriaValorServiceImpl implements CategoriaValorService {
 
     private final CategoriaValorRepository categoriaValorRepository;
+    private final CategoriaService categoriaService;
+    private final ModelMapper modelMapper;
 
-    public CategoriaValorServiceImpl(CategoriaValorRepository categoriaValorRepository) {
+    public CategoriaValorServiceImpl(CategoriaValorRepository categoriaValorRepository, CategoriaService categoriaService, ModelMapper modelMapper) {
         this.categoriaValorRepository = categoriaValorRepository;
+        this.categoriaService = categoriaService;
+        this.modelMapper = modelMapper;
     }
 
     @Override
@@ -26,10 +36,20 @@ public class CategoriaValorServiceImpl implements CategoriaValorService {
         return categoriaValorRepository.findByFechaBajaNull();
     }
 
-
     @Override
-    public CategoriaValor save(CategoriaValor categoriaValor) {
-        return categoriaValorRepository.save(categoriaValor);
+    public MostrarCategoriaValorDTO save(CrearValorDTO valorDTO) {
+        Categoria categoria = categoriaService.findOne(valorDTO.getIdCategoria())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found"));
+        CategoriaValor valor = new CategoriaValor();
+        valor.setNombre(valorDTO.getNombre());
+        valor.setCategoria(categoria);
+        valor = categoriaValorRepository.save(valor);
+        MostrarCategoriaValorDTO retorna = modelMapper.map(valor, MostrarCategoriaValorDTO.class);
+        log.info(valorDTO.toString());
+        log.info(categoria.toString());
+        log.info(valor.toString());
+        log.info(retorna.toString());
+        return retorna;
     }
 
     @Override
@@ -42,9 +62,9 @@ public class CategoriaValorServiceImpl implements CategoriaValorService {
     @Override
     public Optional<CategoriaValor> delete(Long id) {
         Optional<CategoriaValor> categoriaValorOptional = categoriaValorRepository.findById(id);
-        if(categoriaValorOptional.isPresent()) {
+        if (categoriaValorOptional.isPresent()) {
             CategoriaValor categoriaValor = categoriaValorOptional.get();
-            if(categoriaValor.getFechaBaja() != null)
+            if (categoriaValor.getFechaBaja() != null)
                 categoriaValorOptional = Optional.empty();
             else {
                 categoriaValor.setFechaBaja(Instant.now());
