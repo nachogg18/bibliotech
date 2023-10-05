c98363f2b00356425041d470275a8f7f9949fa1f
Ignacio Galvez <iggatsc@gmail.com>
2023-10-05 19:52:18 -0300

    add endpoint get user info

---
+++ src/main/java/com/bibliotech/security/dao/response/RoleDto.java
@@ -0,0 +1,15 @@
+package com.bibliotech.security.dao.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleDto {
+    private Long roleId;
+    private String roleName;
+}
