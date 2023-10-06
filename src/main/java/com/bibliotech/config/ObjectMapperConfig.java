853f2abc1610d696491ab3d669db7d649be26220
Ignacio Galvez <iggatsc@gmail.com>
2023-10-05 21:38:19 -0300

    set object mapper

---
+++ src/main/java/com/bibliotech/config/ObjectMapperConfig.java
@@ -0,0 +1,19 @@
+package com.bibliotech.config;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+}
