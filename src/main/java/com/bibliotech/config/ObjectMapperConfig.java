2bff67f82ced189f67de97697259f4196427d1c8
Ignacio Galvez <iggatsc@gmail.com>
2023-10-05 22:04:52 -0300

    instant representation as iso 8601

--- src/main/java/com/bibliotech/config/ObjectMapperConfig.java
+++ src/main/java/com/bibliotech/config/ObjectMapperConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
 @Configuration
@@ -14,6 +15,9 @@ public class ObjectMapperConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
         objectMapper.registerModule(new JavaTimeModule());
+
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
         return objectMapper;
     }
 }
