7e64919a05489761c40f68b55576d8b3822f9dad
Ignacio Galvez <iggatsc@gmail.com>
2023-10-07 04:18:20 -0300

    fix hasAnyFieldFilled

--- src/main/java/com/bibliotech/dto/FindPublicacionesByParamsDTO.java
+++ src/main/java/com/bibliotech/dto/FindPublicacionesByParamsDTO.java
@@ -8,9 +8,9 @@ public record FindPublicacionesByParamsDTO(String titulo, String autor, String i
   // TODO mejorar validacion de request
   Boolean hasAnyFieldFilled() {
     return StringUtils.isNotBlank(titulo)
-        && StringUtils.isNotBlank(autor)
-        && StringUtils.isNotBlank(isbn)
-        && StringUtils.isNotBlank(anio);
+        || StringUtils.isNotBlank(autor)
+        || StringUtils.isNotBlank(isbn)
+        || StringUtils.isNotBlank(anio);
   }
 
   public void validate() {
