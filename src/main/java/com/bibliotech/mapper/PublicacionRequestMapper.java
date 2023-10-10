c6b3bcf669d01f5a79a83d995f02ffb243bcd023
Sergio Villanueva <65696419+hexdump95@users.noreply.github.com>
2023-10-10 18:13:35 -0300

    Update PublicacionRequestMapper.java

--- src/main/java/com/bibliotech/mapper/PublicacionRequestMapper.java
+++ src/main/java/com/bibliotech/mapper/PublicacionRequestMapper.java
@@ -13,7 +13,7 @@ public class PublicacionRequestMapper {
                     PublicacionResponseDTO dto = new PublicacionResponseDTO();
                     dto.setId(publicacion.getId());
                     dto.setTitulo(publicacion.getTitulo());
-                    dto.setAutores(publicacion.getAutores().stream().map(a -> dto.getNombreAutores().add(a.getApellido().toUpperCase() + ", " + a.getNombre())).toList());
+                    dto.setAutores(publicacion.getAutores().stream().map(a -> a.getApellido().toUpperCase() + ", " + a.getNombre()).toList());
                     dto.setEditoriales(publicacion.getEditoriales().stream().map(Editorial::getNombre).toList());
                     dto.setAnio(publicacion.getAnio());
                     dto.setEdicion(publicacion.getEdicion().getNombre());
