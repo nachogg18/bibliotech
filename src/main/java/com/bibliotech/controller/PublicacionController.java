012b63ddbf9178813efbec3935a4775e0b08a216
Ignacio Galvez <iggatsc@gmail.com>
2023-10-10 15:04:11 -0300

    fix find by params

--- src/main/java/com/bibliotech/controller/PublicacionController.java
+++ src/main/java/com/bibliotech/controller/PublicacionController.java
@@ -1,11 +1,13 @@
 package com.bibliotech.controller;
 
 import com.bibliotech.dto.*;
+import com.bibliotech.entity.Autor;
 import com.bibliotech.entity.Publicacion;
 import com.bibliotech.service.PublicacionService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
@@ -19,47 +21,62 @@ import org.springframework.web.bind.annotation.*;
 @SecurityRequirement(name = "bearer-key")
 @RequiredArgsConstructor
 public class PublicacionController {
-    private final PublicacionService publicacionService;
+  private final PublicacionService publicacionService;
 
-    @PostMapping(path = "/findByParams")
-    @PreAuthorize("@authenticationService.hasPrivilegeOfDoActionForResource('READ', 'PUBLICACION')")
-    public List<Publicacion> findByParams(@RequestBody FindPublicacionesByParamsDTO request) {
+  @PostMapping(path = "/findByParams")
+  @PreAuthorize("@authenticationService.hasPrivilegeOfDoActionForResource('READ', 'PUBLICACION')")
+  public List<PublicacionResponseDTO> findByParams(
+      @RequestBody FindPublicacionesByParamsDTO request) {
 
-        request.validate();
+    request.validate();
 
-        return publicacionService.findByParams(request);
-    }
+    return publicacionService.findByParams(request).stream()
+        .map(
+            publicacion ->
+                PublicacionResponseDTO.builder()
+                    .anioPublicacion(publicacion.getAnio().intValue())
+                    .nombreAutores(
+                        publicacion.getAutores().stream()
+                            .map(Autor::getApellido)
+                            .collect(Collectors.toList()))
+                    .nombreEdicion(publicacion.getEdicion().getNombre())
+                    .tituloPublicacion(publicacion.getTitulo())
+                    .anioPublicacion(publicacion.getAnio())
+                    .build())
+        .collect(Collectors.toList());
+  }
 
-    @PostMapping(path = "")
-    @PreAuthorize("@authenticationService.hasPrivilegeOfDoActionForResource('CREATE', 'PUBLICACION')")
-    public ResponseEntity<PublicacionResponseDTO> create(@RequestBody @Valid CreatePublicacionRequestDTO request) {
+  @PostMapping(path = "")
+  @PreAuthorize("@authenticationService.hasPrivilegeOfDoActionForResource('CREATE', 'PUBLICACION')")
+  public ResponseEntity<PublicacionResponseDTO> create(
+      @RequestBody @Valid CreatePublicacionRequestDTO request) {
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(publicacionService.create(request));
+    return ResponseEntity.status(HttpStatus.OK).body(publicacionService.create(request));
+  }
 
-    }
+  @GetMapping
+  @PreAuthorize("@authenticationService.hasPrivilegeOfDoActionForResource('READ', 'PUBLICACION')")
+  public List<Publicacion> findAll() {
+    return publicacionService.findAll();
+  }
 
-    @GetMapping
-    @PreAuthorize("@authenticationService.hasPrivilegeOfDoActionForResource('READ', 'PUBLICACION')")
-    public List<Publicacion> findAll(){
-        return publicacionService.findAll();
-    }
+  @GetMapping(path = "/paged")
+  @PreAuthorize("@authenticationService.hasPrivilegeOfDoActionForResource('READ', 'PUBLICACION')")
+  public ResponseEntity<PageDTO<PublicacionPaginadaDTO>> getAllPublicacionPaged(
+      @RequestParam(defaultValue = "1") int page) {
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(publicacionService.findAllPublicacionPaginatedDTO(page));
+  }
 
-    @GetMapping(path = "/paged")
-    @PreAuthorize("@authenticationService.hasPrivilegeOfDoActionForResource('READ', 'PUBLICACION')")
-    public ResponseEntity<PageDTO<PublicacionPaginadaDTO>> getAllPublicacionPaged(@RequestParam(defaultValue = "1") int page) {
-        return ResponseEntity.status(HttpStatus.OK).body(publicacionService.findAllPublicacionPaginatedDTO(page));
-    }
+  @GetMapping(path = "/{id}")
+  @PreAuthorize("@authenticationService.hasPrivilegeOfDoActionForResource('READ', 'PUBLICACION')")
+  public ResponseEntity<DetallePublicacionDTO> getDetallePublicacion(@PathVariable Long id) {
+    return ResponseEntity.status(HttpStatus.OK).body(publicacionService.getDetallePublicacion(id));
+  }
 
-    @GetMapping(path = "/{id}")
-    @PreAuthorize("@authenticationService.hasPrivilegeOfDoActionForResource('READ', 'PUBLICACION')")
-    public ResponseEntity<DetallePublicacionDTO> getDetallePublicacion(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(publicacionService.getDetallePublicacion(id));
-    }
-
-    @PatchMapping(path= "/{id}")
-    public ResponseEntity<?> updatePublicacion(@RequestBody ModificarPublicacionDTO req, @PathVariable Long id) {
-        return ResponseEntity.ok().body(publicacionService.updatePublicacion(req, id));
-    }
-
+  @PatchMapping(path = "/{id}")
+  public ResponseEntity<?> updatePublicacion(
+      @RequestBody ModificarPublicacionDTO req, @PathVariable Long id) {
+    return ResponseEntity.ok().body(publicacionService.updatePublicacion(req, id));
+  }
 }
