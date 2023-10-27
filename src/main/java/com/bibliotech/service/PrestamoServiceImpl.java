0f44e8864f075c27e13dc8b89ec1100c3be6aa56
juanrojas216 <juanpablorojas216@gmail.com>
2023-10-26 22:49:58 -0300

    get user para prestamo

--- src/main/java/com/bibliotech/service/PrestamoServiceImpl.java
+++ src/main/java/com/bibliotech/service/PrestamoServiceImpl.java
@@ -63,7 +63,7 @@ public class PrestamoServiceImpl extends BaseServiceImpl<Prestamo, Long> impleme
         return DetallePrestamoDTO.builder()
                 .nombreApellidoUsuario(prestamo.getUsuario().getFirstName() + " " + prestamo.getUsuario().getLastName())
                 .idUsuario(prestamo.getUsuario().getId())
-                //.dniUsuario(prestamo.getUsuario().getDni())
+                .legajoUsuario(prestamo.getUsuario().getLegajo())
                 .tituloPublicacion(prestamo.getEjemplar().getPublicacion().getTitulo())
                 .idEjemplar(prestamo.getEjemplar().getId())
                 .fechaInicioPrestamo(prestamo.getFechaInicioEstimada())
