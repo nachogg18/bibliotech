798cacbb296d41c66be31366b27162c2c1293080
Ignacio Valente <ignacio.valente@alumnos.frm.utn.edu.ar>
2023-10-29 20:45:16 -0300

    Update ComentarioServiceImpl.java

--- src/main/java/com/bibliotech/service/ComentarioServiceImpl.java
+++ src/main/java/com/bibliotech/service/ComentarioServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
 import com.bibliotech.security.entity.User;
 import com.bibliotech.security.service.AuthenticationService;
