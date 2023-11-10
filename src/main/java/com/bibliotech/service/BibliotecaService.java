beba583cdf8bf728cbf93f94faac4023dba66374
Ignacio Valente <ignacio.valente@alumnos.frm.utn.edu.ar>
2023-11-09 22:30:46 -0300

    Update Biblioteca Service find one

--- src/main/java/com/bibliotech/service/BibliotecaService.java
+++ src/main/java/com/bibliotech/service/BibliotecaService.java
@@ -5,6 +5,8 @@ import com.bibliotech.entity.Biblioteca;
 import java.util.List;
 
 public interface BibliotecaService {
+
+    Biblioteca findOne(Long id);
     List<Biblioteca> findAll();
     Biblioteca save(Biblioteca biblioteca);
 }
