ace0db993a430e3965f3f4575a36e31c09bf9bb6
Ignacio Valente <ignacio.valente@alumnos.frm.utn.edu.ar>
2023-11-12 00:37:56 -0300

    Update ejmplar repository with search by nfc mobile

--- src/main/java/com/bibliotech/repository/EjemplarRepository.java
+++ src/main/java/com/bibliotech/repository/EjemplarRepository.java
@@ -32,4 +32,34 @@ public interface EjemplarRepository extends BaseRepository<Ejemplar, Long> {
             "WHERE ejemplar.fecha_baja IS NULL AND ejemplar_estado.fecha_fin IS NULL")
     List<Map<String, Object>> obtenerReporteExistencias();
 
+    @Query(value = "SELECT ej.id as id_ejemplar, " +
+            "       COALESCE(AVG(com.calificacion), 10) as valoracion, " +
+            "       ub.descripcion as ubicacion, " +
+            "       bi.nombre as biblioteca, " +
+            "       p.id as id_publicacion, " +
+            "       p.titulo, " +
+            "       p.sinopsis, " +
+            "       p.anio, " +
+            "       tp.nombre as tipo, " +
+            "       p.isbn, " +
+            "       GROUP_CONCAT(DISTINCT ed.nombre SEPARATOR ';') AS editoriales, " +
+            "       GROUP_CONCAT(DISTINCT CONCAT(a.nombre, ' ', a.apellido) SEPARATOR ';') as autores, " +
+            "       e.nombre " +
+            "FROM ejemplar ej " +
+            "LEFT JOIN ejemplar_comentario ejc ON ej.id = ejc.ejemplar_id " +
+            "LEFT JOIN comentario com ON com.id = ejc.comentario_id " +
+            "LEFT JOIN ubicacion ub ON ej.ubicacion_id = ub.id " +
+            "LEFT JOIN biblioteca bi ON bi.id = ub.biblioteca_id " +
+            "LEFT JOIN publicacion p ON ej.publicacion_id = p.id " +
+            "LEFT JOIN edicion ed ON p.edicion_id = ed.id " +
+            "LEFT JOIN tipo_publicacion tp ON p.tipo_publicacion_id = tp.id " +
+            "LEFT JOIN publicacion_editorial pe ON pe.publicacion_id = p.id " +
+            "LEFT JOIN editorial e ON e.id = pe.editorial_id " +
+            "LEFT JOIN publicacion_autor pa ON pa.publicacion_id = p.id " +
+            "LEFT JOIN autor a ON a.id = pa.autor_id " +
+            "WHERE ej.serialnfc = :serialNfc " +
+            "GROUP BY ej.id, p.id, p.sinopsis, p.titulo, p.anio, tp.nombre, p.isbn, e.nombre, ub.descripcion, bi.nombre",
+            nativeQuery = true)
+    List<Object[]> obtenerEjemplarNFC(@Param("serialNfc") String serialNfc);
+
 }
