23ab861059b1007ea6f87979b189b63e1569eaeb
Ignacio Valente <ignacio.valente@alumnos.frm.utn.edu.ar>
2024-02-05 03:17:41 -0300

    Update ubicacion service impl

--- src/main/java/com/bibliotech/service/UbicacionServiceImpl.java
+++ src/main/java/com/bibliotech/service/UbicacionServiceImpl.java
@@ -1,7 +1,10 @@
 package com.bibliotech.service;
 
+import com.bibliotech.dto.EjemplarDetailDTO;
 import com.bibliotech.dto.UbicacionDTO;
+import com.bibliotech.entity.Ejemplar;
 import com.bibliotech.entity.Ubicacion;
+import com.bibliotech.repository.EjemplarRepository;
 import com.bibliotech.repository.UbicacionRepository;
 import java.time.Instant;
 import java.util.List;
@@ -9,6 +12,7 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -17,6 +21,7 @@ public class UbicacionServiceImpl implements UbicacionService {
 
     private final UbicacionRepository ubicacionRepository;
     private final BibliotecaService bibliotecaService;
+    private final EjemplarRepository ejemplarRepository;
 
     @Override
     public List<Ubicacion> findAll() {
@@ -64,8 +69,7 @@ public class UbicacionServiceImpl implements UbicacionService {
     @Override
     public Ubicacion changeOcupada(Long id, boolean state) {
        Optional<Ubicacion> ubicacion =  ubicacionRepository.findById(id);
-        if (ubicacion.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found");
+        if (ubicacion.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found");
         ubicacion.get().setOcupada(state);
         return ubicacionRepository.save(ubicacion.get());
     }
@@ -75,6 +79,10 @@ public class UbicacionServiceImpl implements UbicacionService {
         Optional<Ubicacion> ubicacionOptional = ubicacionRepository.findById(id);
         if (ubicacionOptional.isPresent()) {
             Ubicacion ubicacion = ubicacionOptional.get();
+            if (ubicacion.isOcupada()){
+                EjemplarDetailDTO ejemplar = this.findOneByUbicacion(ubicacion.getId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No se puede eliminar la ubicación ya que se encuentra ocupada por el ejemplar #"+ejemplar.getId()+" de la publicación "+ejemplar.getNombrePublicacion());
+            }
             if (ubicacion.getFechaBaja() != null)
                 ubicacionOptional = Optional.empty();
             else {
@@ -84,6 +92,19 @@ public class UbicacionServiceImpl implements UbicacionService {
             }
         }
         return ubicacionOptional;
+    }
+
+    public EjemplarDetailDTO findOneByUbicacion(Long id) {
+        Ejemplar ejemplar = ejemplarRepository.findEjemplarByUbicacionId(id).orElseThrow(() -> new RuntimeException(String.format("No existe ejemplar en la ubicación %s", id)));
+        EjemplarDetailDTO dto = new EjemplarDetailDTO();
+        dto.setId(ejemplar.getId());
+        dto.setEjemplarEstadoList(ejemplar.getEjemplarEstadoList());
+        dto.setNombrePublicacion(ejemplar.getPublicacion().getTitulo());
+        dto.setIdUbicacion(ejemplar.getUbicacion().getId());
+        dto.setNombreUbicacion(ejemplar.getUbicacion().getBiblioteca().getNombre() + " - " + ejemplar.getUbicacion().getDescripcion());
+        dto.setSerialNFC(ejemplar.getSerialNFC());
+
+        return dto;
     }
 
     @Override
